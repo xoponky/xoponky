1c4b7cc149a91ed68929a7ff76ee9c4aa38d6d25
Petr Baroš <pbaros@gmail.com>
2016-09-11 13:51:39 +0200

    added core project and unit test

---
+++ xoponky.core.tests/DummyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Xoponky.Core
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Sum_Pass()
+        {
+            //Arrange
+            int v1 = 1, v2 = 2, v3 = 3;
+            var sut = new Dummy();
+
+            //Act
+            var result = sut.Sum(v1, v2, v3);
+
+            //Assert
+            Assert.Equal(6, result);
+        }
+    }
+}
